b3edb68437cd5f937511adcb366d449c4aa7b8d6
Isharaka Gunasinghe <isharaka@yahoo.com>
2018-02-03 20:37:14 +1100

    corrected predicted vehicle s values for track wrap around

--- src/behavior.cpp
+++ src/behavior.cpp
@@ -7,19 +7,29 @@ Behavior::Behavior():_target{1,20.0}
 struct Behavior::target Behavior::generateBehavior(Car& ego, map<int, Car>& cars, Map * track)
 {
     bool too_close = false;  
-    int lane = track->getLane(ego._d_predicted[0]);        
+    int lane = track->getLane(ego._d_predicted[0]); 
+    double ego_predicted_s = ego._s_predicted[0];
+    double ego_s = ego._s[0];     
+
+    for (std::map<int,Car>::iterator it=cars.begin(); it!=cars.end(); ++it) {
+        Car car = it->second;
+
+        if (ego_predicted_s > (car._s_predicted[0] + 0.5*track->max_s))
+            car._s_predicted[0] += track->max_s;
+
+        if (ego_s > (car._s[0] + 0.5*track->max_s))
+            car._s[0] += track->max_s;
+
+        cars[it->first] = car;
+    }  
 
     for (std::map<int,Car>::iterator it=cars.begin(); it!=cars.end(); ++it) {
         Car car = it->second;
 
         if (car._d_predicted[0] < track->getD(lane) + 2 && car._d_predicted[0] > track->getD(lane) - 2) {
-            double ego_s = ego._s_predicted[0];
-            double lead_s = car._s_predicted[0];
+            double car_predicted_s = car._s_predicted[0];
 
-            if (ego_s > (lead_s + 0.5*track->max_s))
-                lead_s += track->max_s;
-
-            if (lead_s > ego_s && (lead_s- ego_s) < 30) {
+            if (car_predicted_s > ego_predicted_s && (car_predicted_s- ego_predicted_s) < 30) {
                 too_close = true;
             }
         }
--- src/behavior.h
+++ src/behavior.h
@@ -1,6 +1,7 @@
 #include <vector>
 #include "prediction.h"
 #include "map.h"
+#include "car.h"
 
 class Behavior
 {
@@ -16,5 +17,14 @@ public:
 
 
 private:
+    enum state {
+        KEEP_LANE = 0,
+        PREPARE_CHANGE_LANE_LEFT = 1,
+        PREPARE_CHANGE_LANE_RIGHT = 2,
+        CHANGE_LANE_LEFT = 3,
+        CHANGE_LANE_RIGHT = 4,
+    };
+
     struct target _target;
+    enum state _state;
 };
